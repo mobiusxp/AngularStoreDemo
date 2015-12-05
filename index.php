79faf4c73e50c0d6803f737bebaa9edfdaf79f65
Ira <ip9636@g.rit.edu>
2015-12-05 14:59:11 -0500

    Initializing repo

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Angular Tutorial</title>
+</head>
+<body>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
