9cf4e29a1dee0b6eaa5b3ab175d418054395ad71
Ira <ip9636@g.rit.edu>
2015-12-07 18:31:16 -0500

    added review controller

---
+++ app.js
@@ -0,0 +1,45 @@
+
+(function(){
+var app = angular.module('myapp', ['ngRoute']);
+
+app.controller('ProductListController',function($scope,$http) {
+    
+    $scope.productList=[];
+    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id=0').success(function(data){
+        console.log("in get");
+        console.dir(data);
+        $scope.productList = data;
+        $scope.x =$scope.productList;
+
+    }); 
+    
+this.gotoDetails = function(id) {
+        //redirect to details
+    console.log("HELLO"+id);     
+    window.location.href = "details.php"
+    };
+                                
+}]);
+    
+app.controller('ProductDetailsController',function($scope,$http) {
+   $scope.product={};
+    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id=1').success(function(data){
+        console.log("in get");
+        console.dir(data);
+        $scope.product = data;
+        
+    }); 
+                           
+});
+    
+app.controller('ReviewController',function($scope) {
+    $scope.userReview="hi";
+    $scope.email="";
+    $scope.addReview = function() {
+        console.log("hello");
+    };
+                                
+});
+    
+
+})();
