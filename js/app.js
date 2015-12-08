5a0ce50aff701f290e011da5919975e1f47c4652
Sana Farooqui <sanafarooqui@sanas-mbp.wireless.rit.edu>
2015-12-07 19:07:35 -0500

    populated images for different ids

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 
 (function(){
+    
+var val = window.location.search.replace("?", "");
+    
+var id = parseInt(val.substring(3,5));
+    
 var app = angular.module('myapp', ['ngRoute']);
 
 app.controller('ProductListController',function($scope,$http) {
     
     $scope.productList=[];
-    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id=0').success(function(data){
+    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id='+id).success(function(data){
         console.log("in get");
         console.dir(data);
         $scope.productList = data;
@@ -16,14 +21,14 @@ app.controller('ProductListController',function($scope,$http) {
 this.gotoDetails = function(id) {
         //redirect to details
     console.log("HELLO"+id);     
-    window.location.href = "details.php"
+    window.location.href = "detail.html?id="+id;
     };
                                 
 });
     
 app.controller('ProductDetailsController',function($scope,$http) {
    $scope.product={};
-    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id=1').success(function(data){
+    $http.get('http://kelvin.ist.rit.edu/~ip9636/Angular/index.php?id='+id).success(function(data){
         console.log("in get");
         console.dir(data);
         $scope.product = data;
@@ -40,6 +45,21 @@ app.controller('ReviewController',function($scope) {
     };
                                 
 });
+    
+function parse(val) {
+    var result = "Not found",
+        tmp = [];
+    location.search
+    //.replace ( "?", "" ) 
+    // this is better, there might be a question mark inside
+    .substr(1)
+        .split("&")
+        .forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
     
 
 })();
